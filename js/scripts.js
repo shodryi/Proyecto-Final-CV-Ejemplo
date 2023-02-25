d0119f5269973a8608fd16b93c738712ceb906a4
rodry <rodriwither@gmail.com>
2023-02-25 16:36:42 -0300

    classes, estilos y scripts simplificados

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,15 @@
-var contenedorExperiencia = document.getElementById("contenedorExperiencia");
-var botonHabilidades = document.getElementById("botonHabilidades");
-var botonExperiencia = document.getElementById("botonExperiencia");
-var botonEducacion = document.getElementById("botonEducacion");
+var botones = document.querySelectorAll(".boton_desplegable");
+var listas = document.querySelectorAll(".lista_desplegable");
+var simbolos = document.querySelectorAll(".simbolo_desplegable");
 
-var listas = [document.getElementById("listaHabilidades"), document.getElementById("listaExperiencia"), document.getElementById("listaEducacion")];
-
-var simbolos = [document.getElementById("simboloHabilidades"), document.getElementById("simboloExperiencia"), document.getElementById("simboloEducacion")];
-
-function mostrarOcultarLista(numero) {
-  if (listas[numero].style.display === "flex") {
-    listas[numero].style.display = "none";
-  } else {
-    listas[numero].style.display = "flex";
-  }
-  cambioDeSimbolo(numero);
-}
-
-function cambioDeSimbolo(numero){
-  if(simbolos[numero].innerHTML === '+'){
-    simbolos[numero].innerHTML = '-';
-  } else {
-    simbolos[numero].innerHTML = '+';
-  }
-}
-
-botonHabilidades.addEventListener("click", function() {
-  mostrarOcultarLista(0);
-});
-
-botonExperiencia.addEventListener("click", function() {
-  mostrarOcultarLista(1);
-});
-
-botonEducacion.addEventListener("click", function() {
-  mostrarOcultarLista(2);
+botones.forEach(function(boton, indice) {
+  boton.addEventListener("click", function() {
+    if (listas[indice].style.display === "flex") {
+      listas[indice].style.display = "none";
+      simbolos[indice].innerHTML = "+";
+    } else {
+      listas[indice].style.display = "flex";
+      simbolos[indice].innerHTML = "-";
+    }
+  });
 });
